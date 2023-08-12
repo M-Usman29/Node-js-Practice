400b07a70878ca3111d287db56d4dbbce323357d
M-Usman29 <usmanamir376@gmail.com>
2023-08-12 17:00:58 +0500

    Update pathmodule.js

--- pathmodule.js
+++ pathmodule.js
@@ -2,8 +2,8 @@ const path = require('node:path');
 
 
 
-//const a= path.dirname('C:\\temp\\myfile.html');
-//console.log(a)
+const a= path.dirname('C:\\temp\\myfile.html');
+console.log(a)
 
 const a2=path.extname('C:\\temp\\myfile.html');
 console.log(a2)
