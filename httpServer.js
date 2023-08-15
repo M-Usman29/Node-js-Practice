2039878013212fe610e474a5fc83207b7ac70da0
M-Usman29 <usmanamir376@gmail.com>
2023-08-15 19:24:33 +0500

    learning

--- httpServer.js
+++ httpServer.js
@@ -1,8 +1,22 @@
 const http = require("http");
+const fs =require('fs');
 
-const port= process.env.port;
+const port= process.env.PORT || 3000;
 
-const server= http.createServer((rq,res)=>{
+const server= http.createServer((req,res)=>{
 
     res.statusCode=200;
+    res.setHeader('Content-Type','Text/html')
+    console.log(req.url)
+    if (req.url=='/'){
+        res.statusCode=200;
+        const data= fs.readFileSync("index.html");
+        res.end(data.toString())
+    }
+
 });
+
+
+server.listen(port,()=>{
+    console.log('Server is lstening on port ',port);
+})
